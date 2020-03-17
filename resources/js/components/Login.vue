bb75371feb2327fe0548829fc02629f63152dd07
José Baptista <jhbaptista2016@gmail.com>
2020-03-16 21:35:54 -0300

    Vue installation

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card card-signin my-5">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Login</h5>
+                        <form action="/api/auth/login" method="POST">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1"><span><i class="fas fa-envelope"></i></span> Email</label>
+                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Insira seu email..." name="email">
+                                <small id="emailHelp" class="form-text text-muted">Nós nunca iremos compartilhar seu email com mais ninguém.</small>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1"><span><i class="fas fa-lock"></i></span> Senha</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Insira sua senha..." name="password">
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <button type="submit" class="btn btn-primary"><span><i class="fas fa-check"></i></span> Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/e1de0b548e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="app">
+        <navbar></navbar>
+        <login></login>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
